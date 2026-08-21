460303fb356bce35311db7abdc81ca712aa93e16
icmicharis <charisministriesintl22@gmail.com>
2026-08-21 13:44:07 +0300

    Fix migration 070: use get_my_role() instead of bare role column in RLS policies

--- supabase/migrations/070_superadmin_sync.sql
+++ supabase/migrations/070_superadmin_sync.sql
@@ -30,6 +30,7 @@ BEGIN
     p_policy, p_table, p_cmd, p_using);
 END;
 $$;
+
 -- 4. Patch core tables
 SELECT _patch_rls_add_superadmin('promotion_history', 'promotion_history_school_isolation', 'ALL',
   '(school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')');
@@ -56,9 +57,9 @@ SELECT _patch_rls_add_superadmin('timetable_slots', 'tt_school_isolation', 'ALL'
 SELECT _patch_rls_add_superadmin('exam_uploads', 'exam_uploads_school_isolation', 'ALL',
   '(school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')');
 
--- Grades
+-- Grades (no role column on grades - uses subquery to profiles)
 SELECT _patch_rls_add_superadmin('grades', 'grades_staff_all', 'ALL',
-  '(((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')) AND (role IN (''teacher'', ''hod'', ''class_teacher'', ''admin'', ''deputy_administrator'', ''bursar'', ''registrar'', ''superadmin'')))');
+  '(school_id = get_my_school_id() AND get_my_role() IN (''admin'', ''teacher'', ''hod'', ''deputy_admin'', ''class_teacher'')) OR (get_my_role() = ''superadmin'')');
 
 -- Messages
 SELECT _patch_rls_add_superadmin('parent_messages', 'parent_messages_school_isolation', 'ALL',
@@ -66,13 +67,13 @@ SELECT _patch_rls_add_superadmin('parent_messages', 'parent_messages_school_isol
 SELECT _patch_rls_add_superadmin('class_comments', 'class_comments_school_isolation', 'ALL',
   '(school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')');
 
--- CBC
+-- CBC (no role column - uses subquery)
 SELECT _patch_rls_add_superadmin('cbc_assessments', 'cbc_assessments_staff_all', 'ALL',
-  '(((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')) AND (role IN (''teacher'', ''hod'', ''class_teacher'', ''admin'', ''deputy_administrator'', ''bursar'', ''registrar'', ''superadmin'')))');
+  '(school_id = get_my_school_id() AND get_my_role() IN (''admin'', ''teacher'', ''hod'', ''deputy_admin'', ''class_teacher'')) OR (get_my_role() = ''superadmin'')');
 
--- Notices
+-- Notices (no role column - uses subquery)
 SELECT _patch_rls_add_superadmin('notices', 'notices_staff_all', 'ALL',
-  '(((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')) AND (role IN (''teacher'', ''hod'', ''class_teacher'', ''admin'', ''deputy_administrator'', ''bursar'', ''registrar'', ''reception'', ''librarian'', ''superadmin'')))');
+  '(school_id = get_my_school_id() AND get_my_role() IN (''teacher'', ''hod'', ''class_teacher'', ''admin'', ''deputy_administrator'', ''bursar'', ''registrar'', ''reception'', ''librarian'')) OR (get_my_role() = ''superadmin'')');
 
 -- Reception
 SELECT _patch_rls_add_superadmin('visitors', 'visitors_school_isolation', 'ALL',
@@ -91,6 +92,7 @@ SELECT _patch_rls_add_superadmin('non_teaching_staff', 'Staff see own school non
   '(school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')');
 SELECT _patch_rls_add_superadmin('departments', 'departments_same_school', 'ALL',
   '(school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')');
+
 -- Library
 SELECT _patch_rls_add_superadmin('library_categories', 'library_categories_school', 'ALL',
   '(school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')');
@@ -113,34 +115,35 @@ SELECT _patch_rls_add_superadmin('library_book_copies', 'library_book_copies_sch
 SELECT _patch_rls_add_superadmin('library_fines', 'library_fines_school', 'ALL',
   '(school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')');
 
--- Accounting (036)
+-- Accounting (036) - these tables have no role column, use get_my_role()
 SELECT _patch_rls_add_superadmin('chart_of_accounts', 'coa_finance_all', 'ALL',
-  '(((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')) AND (role IN (''admin'', ''bursar'', ''deputy_administrator'', ''superadmin'')))');
+  '((school_id = get_my_school_id()) AND (get_my_role() IN (''admin'', ''bursar'', ''deputy_administrator''))) OR (get_my_role() = ''superadmin'')');
 SELECT _patch_rls_add_superadmin('chart_of_accounts', 'coa_staff_select', 'SELECT',
-  '((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin''))');
+  '(school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')');
 SELECT _patch_rls_add_superadmin('journal_entries', 'je_finance_all', 'ALL',
-  '(((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')) AND (role IN (''admin'', ''bursar'', ''deputy_administrator'', ''superadmin'')))');
+  '((school_id = get_my_school_id()) AND (get_my_role() IN (''admin'', ''bursar'', ''deputy_administrator''))) OR (get_my_role() = ''superadmin'')');
 SELECT _patch_rls_add_superadmin('journal_entries', 'je_staff_select', 'SELECT',
-  '((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin''))');
+  '(school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')');
 SELECT _patch_rls_add_superadmin('journal_entry_lines', 'jel_finance_all', 'ALL',
   '((get_my_role() = ''superadmin'') OR ((SELECT school_id FROM journal_entries WHERE id = journal_entry_id) = get_my_school_id()))');
 SELECT _patch_rls_add_superadmin('journal_entry_lines', 'jel_staff_select', 'SELECT',
   '((get_my_role() = ''superadmin'') OR ((SELECT school_id FROM journal_entries WHERE id = journal_entry_id) = get_my_school_id()))');
 SELECT _patch_rls_add_superadmin('fiscal_periods', 'fp_finance_all', 'ALL',
-  '(((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')) AND (role IN (''admin'', ''bursar'', ''deputy_administrator'', ''superadmin'')))');
+  '((school_id = get_my_school_id()) AND (get_my_role() IN (''admin'', ''bursar'', ''deputy_administrator''))) OR (get_my_role() = ''superadmin'')');
 SELECT _patch_rls_add_superadmin('fiscal_periods', 'fp_staff_select', 'SELECT',
-  '((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin''))');
+  '(school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')');
 
 -- Payroll account mapping (040)
 SELECT _patch_rls_add_superadmin('payroll_account_mapping', 'pam_finance_all', 'ALL',
-  '(((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')) AND (role IN (''admin'', ''bursar'', ''deputy_administrator'', ''superadmin'')))');
+  '((school_id = get_my_school_id()) AND (get_my_role() IN (''admin'', ''bursar'', ''deputy_administrator''))) OR (get_my_role() = ''superadmin'')');
 SELECT _patch_rls_add_superadmin('payroll_account_mapping', 'pam_staff_select', 'SELECT',
-  '((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin''))');
+  '(school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')');
+
 -- Dynamic-loop finance tables (037, 039, 043, 045, 050, 051)
 DO $$ DECLARE t text; BEGIN
   FOREACH t IN ARRAY ARRAY['asset_categories','suppliers','fixed_assets','asset_events','asset_custody_history','asset_location_history','asset_maintenance','asset_depreciation_runs','asset_depreciation_lines','asset_documents'] LOOP
     EXECUTE format('DROP POLICY IF EXISTS fin_all_%s ON %I', t, t);
-    EXECUTE format('CREATE POLICY fin_all_%s ON %I FOR ALL USING (((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')) AND (role IN (''admin'', ''bursar'', ''deputy_administrator'', ''superadmin'')))', t, t);
+    EXECUTE format('CREATE POLICY fin_all_%s ON %I FOR ALL USING ((school_id = get_my_school_id() AND get_my_role() IN (''admin'',''bursar'',''deputy_administrator'')) OR (get_my_role() = ''superadmin''))', t, t);
     EXECUTE format('DROP POLICY IF EXISTS staff_select_%s ON %I', t, t);
     EXECUTE format('CREATE POLICY staff_select_%s ON %I FOR SELECT USING ((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin''))', t, t);
   END LOOP;
@@ -149,7 +152,7 @@ END $$;
 DO $$ DECLARE t text; BEGIN
   FOREACH t IN ARRAY ARRAY['payroll_statutory_config','payroll_employees','payroll_employee_items','payroll_periods','payroll_runs','payroll_lines','payroll_payment_requests'] LOOP
     EXECUTE format('DROP POLICY IF EXISTS fin_all_%s ON %I', t, t);
-    EXECUTE format('CREATE POLICY fin_all_%s ON %I FOR ALL USING (((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')) AND (role IN (''admin'', ''bursar'', ''deputy_administrator'', ''superadmin'')))', t, t);
+    EXECUTE format('CREATE POLICY fin_all_%s ON %I FOR ALL USING ((school_id = get_my_school_id() AND get_my_role() IN (''admin'',''bursar'',''deputy_administrator'')) OR (get_my_role() = ''superadmin''))', t, t);
     EXECUTE format('DROP POLICY IF EXISTS staff_select_%s ON %I', t, t);
     EXECUTE format('CREATE POLICY staff_select_%s ON %I FOR SELECT USING ((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin''))', t, t);
   END LOOP;
@@ -158,7 +161,7 @@ END $$;
 DO $$ DECLARE t text; BEGIN
   FOREACH t IN ARRAY ARRAY['ap_suppliers','ap_invoices','ap_invoice_lines','ap_tax_config','ap_payments','ap_payment_allocations','finance_attachments'] LOOP
     EXECUTE format('DROP POLICY IF EXISTS fin_all_%s ON %I', t, t);
-    EXECUTE format('CREATE POLICY fin_all_%s ON %I FOR ALL USING (((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')) AND (role IN (''admin'', ''bursar'', ''deputy_administrator'', ''superadmin'')))', t, t);
+    EXECUTE format('CREATE POLICY fin_all_%s ON %I FOR ALL USING ((school_id = get_my_school_id() AND get_my_role() IN (''admin'',''bursar'',''deputy_administrator'')) OR (get_my_role() = ''superadmin''))', t, t);
     EXECUTE format('DROP POLICY IF EXISTS staff_select_%s ON %I', t, t);
     EXECUTE format('CREATE POLICY staff_select_%s ON %I FOR SELECT USING ((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin''))', t, t);
   END LOOP;
@@ -167,7 +170,7 @@ END $$;
 DO $$ DECLARE t text; BEGIN
   FOREACH t IN ARRAY ARRAY['tax_rules','asset_tax_schedules'] LOOP
     EXECUTE format('DROP POLICY IF EXISTS fin_all_%s ON %I', t, t);
-    EXECUTE format('CREATE POLICY fin_all_%s ON %I FOR ALL USING (((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')) AND (role IN (''admin'', ''bursar'', ''deputy_administrator'', ''superadmin'')))', t, t);
+    EXECUTE format('CREATE POLICY fin_all_%s ON %I FOR ALL USING ((school_id = get_my_school_id() AND get_my_role() IN (''admin'',''bursar'',''deputy_administrator'')) OR (get_my_role() = ''superadmin''))', t, t);
     EXECUTE format('DROP POLICY IF EXISTS staff_select_%s ON %I', t, t);
     EXECUTE format('CREATE POLICY staff_select_%s ON %I FOR SELECT USING ((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin''))', t, t);
   END LOOP;
@@ -176,7 +179,7 @@ END $$;
 DO $$ DECLARE t text; BEGIN
   FOREACH t IN ARRAY ARRAY['expenses','expense_lines'] LOOP
     EXECUTE format('DROP POLICY IF EXISTS fin_all_%s ON %I', t, t);
-    EXECUTE format('CREATE POLICY fin_all_%s ON %I FOR ALL USING (((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')) AND (role IN (''admin'', ''bursar'', ''deputy_administrator'', ''superadmin'')))', t, t);
+    EXECUTE format('CREATE POLICY fin_all_%s ON %I FOR ALL USING ((school_id = get_my_school_id() AND get_my_role() IN (''admin'',''bursar'',''deputy_administrator'')) OR (get_my_role() = ''superadmin''))', t, t);
     EXECUTE format('DROP POLICY IF EXISTS staff_select_%s ON %I', t, t);
     EXECUTE format('CREATE POLICY staff_select_%s ON %I FOR SELECT USING ((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin''))', t, t);
   END LOOP;
@@ -185,7 +188,7 @@ END $$;
 DO $$ DECLARE t text; BEGIN
   FOREACH t IN ARRAY ARRAY['cash_transfers','bank_reconciliations','bank_reconciliation_lines'] LOOP
     EXECUTE format('DROP POLICY IF EXISTS fin_all_%s ON %I', t, t);
-    EXECUTE format('CREATE POLICY fin_all_%s ON %I FOR ALL USING (((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin'')) AND (role IN (''admin'', ''bursar'', ''deputy_administrator'', ''superadmin'')))', t, t);
+    EXECUTE format('CREATE POLICY fin_all_%s ON %I FOR ALL USING ((school_id = get_my_school_id() AND get_my_role() IN (''admin'',''bursar'',''deputy_administrator'')) OR (get_my_role() = ''superadmin''))', t, t);
     EXECUTE format('DROP POLICY IF EXISTS staff_select_%s ON %I', t, t);
     EXECUTE format('CREATE POLICY staff_select_%s ON %I FOR SELECT USING ((school_id = get_my_school_id()) OR (get_my_role() = ''superadmin''))', t, t);
   END LOOP;
